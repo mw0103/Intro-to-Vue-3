35ead3e9e86e538739d75b444d887ab6a9dbbc02
Madison Wilkes <mw0103@uah.edu>
2022-02-14 12:30:40 -0600

    stage 2

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+             products: 'Socks',
+             description : 'A pair of warm, fuzzy socks',
+             image: './assets/images/socks_blue.jpg',
+             url: 'https://www.google.com',
+             }
+    }
+})
